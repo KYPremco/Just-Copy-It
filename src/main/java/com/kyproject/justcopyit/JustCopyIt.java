15c8585dd5493318a1206e7ee67e4fcb023270e6
KYPremco <KYPremco@gmail.com>
2017-11-26 18:17:00 +0100

    -

--- src/main/java/com/kyproject/justcopyit/JustCopyIt.java
+++ src/main/java/com/kyproject/justcopyit/JustCopyIt.java
@@ -15,7 +15,7 @@ import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.network.NetworkRegistry;
 
-@Mod(modid = JustCopyIt.MODID, version = JustCopyIt.VERSION, name = JustCopyIt.NAME, acceptableSaveVersions = "[1.12.2]", updateJSON = "test")
+@Mod(modid = JustCopyIt.MODID, version = JustCopyIt.VERSION, name = JustCopyIt.NAME, acceptableSaveVersions = "[1.12.2]")
 public class JustCopyIt {
     public static final String MODID = "kypjci";
     public static final String VERSION = "0.6";
